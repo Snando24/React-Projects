98ed5dd415813639cd9cc45309d4c4fa320afadc
Jr. SoftNanTec <nandno1408@gmail.com>
2025-03-01 10:16:54 -0500

    Inicio de React

---
+++ project-1/src/index.js
@@ -0,0 +1,8 @@
+const name = 'John Doe';
+let value = 42;
+console.log(`Hello, ${name}!` + value);
+ if (true) {
+    let value = 55;
+    console.log('This is true' + value); 
+}
+console.log(value);
